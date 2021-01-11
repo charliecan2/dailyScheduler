8328c6a774266962963eff5f1f3610465091f961
charliecan2 <carloscq920@gmail.com>
2021-01-11 09:09:00 -0600

    Added comments to the javascript document

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-var container = $(".container");
-var div = $("<div>");
-var textarea = $("textarea");
-var button = $("<button>");
+// Retrieved each individual textarea using ID's in the html and assigned them to variables
 var currentDay = $("#currentDay");
-var hour = $(".hour");
-
 var nineAm = $("#9am").attr("time");
 var tenAm = $("#10am").attr("time");
 var elevenAm = $("#11am").attr("time");
@@ -15,9 +10,11 @@ var threePm = $("#3pm").attr("time");
 var fourPm = $("#4pm").attr("time");
 var fivePm = $("#5pm").attr("time");
 
-var weekday = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// Created array's meant to display the day of the week and month on the page
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+// Declared the new Date object and its methods, assigned them into variables for ease of use
 var d = new Date();
 var getMonth = d.getMonth();
 var getDay = d.getDay();
@@ -25,12 +22,13 @@ var getDate = d.getDate();
 var getYear = d.getFullYear();
 var getHours = d.getHours();
 
+// Assigned text to the paragraph element with ID #currentDay using the above variables and arrays
 currentDay.text(weekday[getDay] + ", " + months[getMonth] + " " + getDate + ", " + getYear);
 
 
 // Compares 9AM timeblock to current hour
 if (+nineAm < getHours) {
-    $("#textarea1").addClass("past");
+     $("#textarea1").addClass("past");
 }
 else if (+nineAm == getHours) {
      $("#textarea1").addClass("present");
@@ -41,7 +39,7 @@ else if (+nineAm > getHours) {
 
 // Compares 10AM timeblock to current hour
 if (+tenAm < getHours) {
-    $("#textarea2").addClass("past");
+     $("#textarea2").addClass("past");
 }
 else if (+tenAm == getHours) {
      $("#textarea2").addClass("present");
@@ -51,8 +49,8 @@ else if (+tenAm > getHours) {
 }
 
 // Compares 11AM timeblock to current hour
-if (+elevenAm< getHours) {
-    $("#textarea3").addClass("past");
+if (+elevenAm < getHours) {
+     $("#textarea3").addClass("past");
 }
 else if (+elevenAm == getHours) {
      $("#textarea3").addClass("present");
@@ -63,7 +61,7 @@ else if (+elevenAm > getHours) {
 
 // Compares 12PM timeblock to current hour
 if (+twelvePm < getHours) {
-    $("#textarea4").addClass("past");
+     $("#textarea4").addClass("past");
 }
 else if (+twelvePm == getHours) {
      $("#textarea4").addClass("present");
@@ -74,7 +72,7 @@ else if (+twelvePm > getHours) {
 
 // Compares 1PM timeblock to current hour
 if (+onePm < getHours) {
-    $("#textarea5").addClass("past");
+     $("#textarea5").addClass("past");
 }
 else if (+onePm == getHours) {
      $("#textarea5").addClass("present");
@@ -85,7 +83,7 @@ else if (+onePm > getHours) {
 
 // Compares 2PM timeblock to current hour
 if (+twoPm < getHours) {
-    $("#textarea6").addClass("past");
+     $("#textarea6").addClass("past");
 }
 else if (+twoPm == getHours) {
      $("#textarea6").addClass("present");
@@ -96,7 +94,7 @@ else if (+twoPm > getHours) {
 
 // Compares 3PM timeblock to current hour
 if (+threePm < getHours) {
-    $("#textarea7").addClass("past");
+     $("#textarea7").addClass("past");
 }
 else if (+threePm == getHours) {
      $("#textarea7").addClass("present");
@@ -107,7 +105,7 @@ else if (+threePm > getHours) {
 
 // Compares 4PM timeblock to current hour
 if (+fourPm < getHours) {
-    $("#textarea8").addClass("past");
+     $("#textarea8").addClass("past");
 }
 else if (+fourPm == getHours) {
      $("#textarea8").addClass("present");
@@ -118,7 +116,7 @@ else if (+fourPm > getHours) {
 
 // Compares 5PM timeblock to current hour
 if (+fivePm < getHours) {
-    $("#textarea9").addClass("past");
+     $("#textarea9").addClass("past");
 }
 else if (+fivePm == getHours) {
      $("#textarea9").addClass("present");
@@ -127,34 +125,37 @@ else if (+fivePm > getHours) {
      $("#textarea9").addClass("future");
 }
 
-$("#button1").on("click", function(){
+// Assigned "on click" event listeners onto each individual button on the HTML document
+// Button's will take the values input into the text areas and store them to Local Storage
+$("#button1").on("click", function () {
      localStorage.setItem("textvalue1", $("#textarea1").val());
 });
-$("#button2").on("click", function(){
+$("#button2").on("click", function () {
      localStorage.setItem("textvalue2", $("#textarea2").val());
 });
-$("#button3").on("click", function(){
+$("#button3").on("click", function () {
      localStorage.setItem("textvalue3", $("#textarea3").val());
 });
-$("#button4").on("click", function(){
+$("#button4").on("click", function () {
      localStorage.setItem("textvalue4", $("#textarea4").val());
 });
-$("#button5").on("click", function(){
+$("#button5").on("click", function () {
      localStorage.setItem("textvalue5", $("#textarea5").val());
 });
-$("#button6").on("click", function(){
+$("#button6").on("click", function () {
      localStorage.setItem("textvalue6", $("#textarea6").val());
 });
-$("#button7").on("click", function(){
+$("#button7").on("click", function () {
      localStorage.setItem("textvalue7", $("#textarea7").val());
 });
-$("#button8").on("click", function(){
+$("#button8").on("click", function () {
      localStorage.setItem("textvalue8", $("#textarea8").val());
 });
-$("#button9").on("click", function(){
+$("#button9").on("click", function () {
      localStorage.setItem("textvalue9", $("#textarea9").val());
 });
 
+// Each individual text area will retrieve text from values input into local storage, if any
 $("#textarea1").text(localStorage.getItem("textvalue1"));
 $("#textarea2").text(localStorage.getItem("textvalue2"));
 $("#textarea3").text(localStorage.getItem("textvalue3"));
